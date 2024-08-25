a7d64099924efbaf5931aa70f6db1d3be5779e45
MithunSAnil <mithunsanil2003@gmail.com>
2024-08-26 01:39:26 +0530

    corrected error in in latinSquareMatrix where l1 = l2 would point to the same matrix

--- PythonPrograms/LatinSquareMatrix.py
+++ PythonPrograms/LatinSquareMatrix.py
@@ -4,6 +4,6 @@ print(*l1)
 l2 = [0 for i in range(n)]
 for i in range(1,n):
     for j in range(n):
-        l2[j] = l1[(j+1)%n]
-    l1 = l2
-    print(*l1)
+        l2[j] = l1[(j+i)%n]
+
+    print(*l2)
