07a48ad0925e7472f0feb15a73e6a755915dd905
MithunSAnil <mithunsanil2003@gmail.com>
2024-09-03 17:13:42 +0530

    Added a3b2c4.py

---
+++ PythonPrograms/a3b2c3d5.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+dict1 = defaultdict(str)
+
+str1 = input()
+
+for ele in str1:
+    if ele.isalpha():
+        now = ele
+    else:
+        dict1[now] += ele
+
+for ele in dict1:
+    print(ele*int(dict1[ele]), end = "")
