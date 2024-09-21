b780788aed9977eec5f3a2812b1ad22bd80fd84a
MithunSAnil <mithunsanil2003@gmail.com>
2024-09-21 20:02:54 +0530

    Completed cricket match problem using greedy algorithm

--- JavaPrograms/GreedyCricketMatch/src/Main.java
+++ JavaPrograms/GreedyCricketMatch/src/Main.java
@@ -1,6 +1,7 @@
+import java.util.Comparator;
 import java.util.Scanner;
-
-class activity{
+import java.util.Arrays;
+class Activity{
     String name;
     int startTime, endTime;
 }
@@ -9,12 +10,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        activity[] arr = new activity[n];
-        for(int i = 0; i<n; i++){
-            sc.nextLine();
-            arr[i].name = sc.nextLine();
+        Activity[] arr = new Activity[n];
+        for(int i = 0; i<n; i++) {
+            arr[i] = new Activity();
+            arr[i].name = sc.next();
             arr[i].startTime = sc.nextInt();
             arr[i].endTime = sc.nextInt();
+            sc.nextLine();
+        }
+        Arrays.sort(arr, Comparator.comparingInt(a -> a.endTime));
+        int currEndTime = arr[0].endTime;
+        System.out.print(arr[0].name + " ");
+        for(int i = 1; i < n; i++) {
+            if(currEndTime < arr[i].startTime) {
+                System.out.print(arr[i].name + " ");
+                currEndTime = arr[i].endTime;
+            }
         }
     }
 }
