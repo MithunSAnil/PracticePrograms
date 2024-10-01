968422bee4f430e97a3a67c444547cfb334d8a3c
MithunSAnil <mithunsanil2003@gmail.com>
2024-10-01 21:11:48 +0530

    Added java notes file

---
+++ JavaPrograms/Notes.java
@@ -0,0 +1,13 @@
+public class Notes
+{
+    public static void main(String[] args)
+    {
+        // Static methods belong to the class and not to a particular object. So they do not need objects to be called
+        public static void addNode()
+        {
+            /*Can be called using ClassName.addNode. There is no need to create an object*/
+        }
+
+        // Public class Main should be named Main only because it is public and needs to be accesses by other classes outside
+    }
+}
